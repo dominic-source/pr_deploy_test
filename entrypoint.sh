0b9c53165d7c15de4fb0685a9a88e00cc378588b
Dominic <chinonsodomnic@gmail.com>
2024-07-29 02:01:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ CONTAINER_NAME="${BRANCH_NAME}_${PR_NUMBER}_container"
 echo "Starting deployment 1..."
 
 # apt install sshpass
+echo $SERVER_PASSWORD
+echo $SERVER_HOST
 sshpass -p $SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $SERVER_USERNAME@$SERVER_HOST <<EOF
   echo "Starting deployment 2..."
   mkdir -p /root/chinonso
